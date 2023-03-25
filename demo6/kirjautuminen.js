6b911c4c47b9aa24e1c331a3aadff39f6b29bc25
JarnoKeuda <126771390+JarnoKeuda@users.noreply.github.com>
2023-03-25 12:18:51 +0200

    ei jumalauta

--- demo6/kirjautuminen.js
+++ demo6/kirjautuminen.js
@@ -3,10 +3,7 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
     if(kirjautunut === 'kylla'){
-        document.getElementById("tervetuloa_teksti").textContent += localStorage.getItem("nimi");
+        document.getElementById("tervetuloa_teksti").textContent += "  jape!";
     }
 }
 
-function kirjaudu(){
-    localStorage.setItem("nimi", "jape");
-}
