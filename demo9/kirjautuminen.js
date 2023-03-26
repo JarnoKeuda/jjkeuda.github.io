a36d8beb179b43aefb9814426a2392da927bf843
JarnoKeuda <126771390+JarnoKeuda@users.noreply.github.com>
2023-03-26 09:12:23 +0300

    g

--- demo9/kirjautuminen.js
+++ demo9/kirjautuminen.js
@@ -4,25 +4,16 @@ function onkoKirjautunut(){
     if(localStorage.getItem("kirjautunut") === 'kylla'){
         document.getElementById("tervetuloa_teksti").textContent += "  " + localStorage.getItem("nimi");
         document.getElementById("kirjautumis_lomake").style.display = "none";
-        document.getElementById("kirjaudu_ulos_nappi").style.display = "inline-block";
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "block";
     }
 }
-
-document.getElementById("kirjaudu_nappi").addEventListener("click", function(e) {
-    e.preventDefault();
-    kirjaudu();
-});
-
-document.getElementById("kirjaudu_ulos_nappi").addEventListener("click", function() {
-    kirjauduUlos();
-});
 
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
-    document.getElementById("kirjaudu_ulos_nappi").style.display = "inline-block";
+    document.getElementById("kirjaudu_nappi").style.display = "none";
+    document.getElementById("kirjaudu_ulos_nappi").style.display = "block";
     document.getElementById("tervetuloa_teksti").textContent = "Tervetuloa " + localStorage.getItem("nimi");
-    document.getElementById("kirjautumis_lomake").style.display = "none";
 }
 
 function kirjauduUlos(){
@@ -30,6 +21,7 @@ function kirjauduUlos(){
     localStorage.removeItem("kirjautunut");
     document.getElementById("kirjautumis_lomake").style.display = "block";
     document.getElementById("nimi").value = "";
+    document.getElementById("kirjaudu_nappi").style.display = "inline-block";
     document.getElementById("kirjaudu_ulos_nappi").style.display = "none";
     document.getElementById("tervetuloa_teksti").textContent = "Tervetuloa";
 }
