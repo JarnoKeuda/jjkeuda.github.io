310cf5060d2a5202bed80dc8ae73b2a36b0fdbaf
JarnoKeuda <126771390+JarnoKeuda@users.noreply.github.com>
2023-03-26 08:38:24 +0300

    demo9

---
+++ demo9/kirjautuminen.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+    if(localStorage.getItem("kirjautunut") === 'kylla'){
+        document.getElementById("tervetuloa_teksti").textContent += "  " + localStorage.getItem("nimi");
+        document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "inline-block";
+    }
+}
+
+function kirjaudu(){
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kylla");
+    document.getElementById("kirjaudu_ulos_nappi").style.display = "inline-block";
+}
+
+function kirjauduUlos(){
+    localStorage.removeItem("nimi");
+    localStorage.removeItem("kirjautunut");
+    document.getElementById("kirjautumis_lomake").style.display = "block";
+    document.getElementById("nimi").value = "";
+    document.getElementById("kirjaudu_ulos_nappi").style.display = "none";
+    document.getElementById("tervetuloa_teksti").textContent = "Tervetuloa";
+}
